5c4a0ba8c877f8be41b9c4daab2c8e2a8af358f1
jacktain <100468139+jacktain@users.noreply.github.com>
2024-06-27 21:53:26 +0800

    Update speed_AIO.sh

--- speed_AIO.sh
+++ speed_AIO.sh
@@ -19,9 +19,9 @@ telegramBotUserId="" # telegram UserId
 telegramBotToken="6599852032:AAHhetLKhXfAIjeXgCHpish1DK_NHo3BCrk" #telegram BotToken https://t.me/ACFST_DDNS_bot
 telegramBotAPI="api.telegram.ssrc.cf" #telegram 推送API,留空将启用官方API接口:api.telegram.org
 
-githubID="cmliu" #自用IP库，也可以换成你自己的github仓库，且仓库名必须是"cloudflare-better-ip" 可自行Fork修改 https://github.com/cmliu/cloudflare-better-ip
+githubID="jacktain" #自用IP库，也可以换成你自己的github仓库，且仓库名必须是"cloudflare-better-ip" 可自行Fork修改 https://github.com/cmliu/cloudflare-better-ip
 ###############################################################以下脚本内容，勿动#######################################################################
-speedurl="https://speed.cloudflare.com/__down?bytes=$((speedtestMB * 1000000))" #官方测速链接
+speedurl="https://cs.jkzz.eu.org/90m" #官方测速链接
 proxygithub="https://mirror.ghproxy.com/" #反代github加速地址，如果不需要可以将引号内容删除，如需修改请确保/结尾 例如"https://mirror.ghproxy.com/"
 CloudFlareIP_password=""
 #带有地区参数，将赋值第1参数为地区
